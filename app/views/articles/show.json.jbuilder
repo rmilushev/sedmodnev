d28df264cae819f1f1b78e1e6c451ad163f88e41
Rumen Milushev <rumen.g.milushev@gmail.com>
2016-06-20 06:16:45 +0300

    Add articles

---
+++ app/views/articles/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @article, :id, :title, :content, :published, :picture, :author_id, :category_id, :golive, :created_at, :updated_at
---
+++ spec/factories/articles.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :article do
+    title "MyString"
+    content "MyText"
+    published false
+    picture "MyString"
+    author_id "9.99"
+    category_id "9.99"
+    golive "2016-06-20 06:16:04"
+  end
+end
