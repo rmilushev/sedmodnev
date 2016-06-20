4e69d60ec4165a1a9089e9740f48131f6ea7cee4
Rumen Milushev <rumen.g.milushev@gmail.com>
2016-06-20 18:55:31 +0300

    Add new git repository for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,49 +1,106 @@
-# config valid only for current version of Capistrano
-lock '3.5.0'
+set :application, 'sedmodnev'
+set :deploy_user, 'deployer'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+# setup repo details
+set :scm, :git
+set :repo_url, 'https://github.com/rmilushev/sedmodnev.git'
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# setup rbenv.
+set :rbenv_type, :system
+set :rbenv_ruby, "2.2.3"
+set :rbenv_path, "~/.rbenv"
+set :default_env, path: "~/.rbenv/shims:~/.rbenv/bin:$PATH"
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
 
-# Default value for :scm is :git
-# set :scm, :git
+set :rbenv_map_bins, %w( rake gem bundle ruby rails )
+# how many old releases do we want to keep, not much
+set :keep_releases, 5
 
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
+# files we want symlinking to specific entries in shared
+set :linked_files, %w( config/database.yml config/application.yml )
 
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
+# dirs we want symlinking to shared
+set :linked_dirs, %w( log tmp/pids tmp/cache tmp/sockets vendor/bundle
+                      public/system uploads
+                      tmp/uploads/store tmp/uploads/cache)
 
-# Default value for :pty is false
-# set :pty, true
+# what specs should be run before deployment is allowed to
+# continue, see lib/capistrano/tasks/run_tests.cap
+set :tests, []
 
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+# which config files should be copied by deploy:setup_config
+# see documentation in lib/capistrano/tasks/setup_config.cap
+# for details of operations
+set(:config_files, %w(
+  nginx.conf
+  database.example.yml
+  application.example.yml
+  unicorn.rb
+  unicorn_init.sh
+))
 
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
+# log_rotation
+# monit
 
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+# which config files should be made executable after copying
+# by deploy:setup_config
+set(:executable_config_files, %w(
+  unicorn_init.sh
+))
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+# files which need to be symlinked to other parts of the
+# filesystem. For example nginx virtualhosts, log rotation
+# init scripts etc. The full_app_name variable isn't
+# available at this point so we use a custom template {{}}
+# tag and then add it at run time.
+set(:symlinks, [
+  {
+    source: 'nginx.conf',
+    link: '/etc/nginx/sites-enabled/{{full_app_name}}'
+  },
+  {
+    source: 'unicorn_init.sh',
+    link: '/etc/init.d/unicorn_{{full_app_name}}'
+  }
+])
+
+# ,
+# {
+#   source: "log_rotation",
+#  link: "/etc/logrotate.d/{{full_app_name}}"
+# },
+# {
+#   source: "monit",
+#   link: "/etc/monit/conf.d/{{full_app_name}}.conf"
+# }
+# this:
+# http://www.capistranorb.com/documentation/getting-started/flow/
+# is worth reading for a quick overview of what tasks are called
+# and when for `cap stage deploy`
 
 namespace :deploy do
+  # make sure we're deploying what we think we're deploying
+  before :deploy, 'deploy:check_revision'
+  # only allow a deploy with passing tests to deployed
+  # before :deploy, "deploy:run_tests"
+  # compile assets locally then rsync
+  after 'deploy:symlink:shared', 'deploy:compile_assets_locally'
+  after :finishing, 'deploy:cleanup'
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
+  # remove the default nginx configuration as it will tend
+  # to conflict with our configs.
+  before 'deploy:setup_config', 'nginx:remove_default_vhost'
 
+  # reload nginx to it will pick up any modified vhosts from
+  # setup_config
+  after 'deploy:setup_config', 'nginx:reload'
+
+  # Restart monit so it will pick up any monit configurations
+  # we've added
+  # after 'deploy:setup_config', 'monit:restart'
+
+  # As of Capistrano 3.1, the `deploy:restart` task is not called
+  # automatically.
+  after 'deploy:publishing', 'deploy:restart'
 end
