70cf198689262e5a12660354a1ec43b44f19bf44
Rumen Milushev <rumen.g.milushev@gmail.com>
2016-06-21 18:43:43 +0300

    Fix fog provider

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,7 +2,7 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     # config.storage = "fog/rackspace/storage"
     config.fog_credentials = {
-      provider:           ENV["carrierwave_provider"],
+      provider:           'Rackspace',
       rackspace_username: ENV["carrierwave_user"],
       rackspace_api_key:  ENV["carrierwave_api_key"],
       rackspace_region:   :lon
