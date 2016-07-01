2c902b01d91dea63ba1a7bc889cf1883dd5c3e5a
Rumen Milushev <rumen.g.milushev@gmail.com>
2016-07-01 21:43:39 +0300

    Clean afrer redactor

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  mount RedactorRails::Engine => '/redactor_rails'
   resources :articles
   devise_for :admins
   root to: 'high_voltage/pages#show', id: 'construction'
