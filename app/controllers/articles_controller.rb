78073f17572abff1fcdf83c1292b79491d2b7549
Rumen Milushev <rumen.g.milushev@gmail.com>
2016-07-02 17:23:54 +0300

    Add error notification for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -32,7 +32,7 @@ class ArticlesController < ApplicationController
         format.html { redirect_to @article, notice: 'Article was successfully created.' }
         format.json { render :show, status: :created, location: @article }
       else
-        format.html { render :new }
+        format.html { render :new, notice: @article.errors }
         format.json { render json: @article.errors, status: :unprocessable_entity }
       end
     end
