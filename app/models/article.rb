6da2049928c57897dc0fee60078abdf5a44cf2e6
Rumen Milushev <rumen.g.milushev@gmail.com>
2016-07-03 13:01:05 +0300

    Order desc for articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,5 @@
 class Article < ActiveRecord::Base
-  default_scope { order('created_at') }
+  default_scope { order('created_at DESC') }
   validates :title, presence: true
   attachment :article_image
 end
