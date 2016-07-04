5aa3df8c60348bc4a060f36149523347111d7999
Rumen Milushev <rumen.g.milushev@gmail.com>
2016-07-04 12:19:26 +0300

    Yeee!!! We go live today!

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :authenticate_admin!
+  before_action :authenticate_admin!, except: [:index, :show]
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   # GET /articles
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :articles
   devise_for :admins
+  devise_scope :admin do
+    match 'admin' => 'devise/sessions#new', via: :get
+  end
   root to: 'articles#index'
   # root to: 'high_voltage/pages#show', id: 'construction'
 end
