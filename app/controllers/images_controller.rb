968ce35dbb790a4dedf5c75ad0eef24cf47e0fa5
Rumen Milushev <rumen.g.milushev@gmail.com>
2016-07-08 11:48:33 +0300

    Add image_params to controller

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,4 +1,20 @@
 class ImagesController < ApplicationController
-  def create
+  def new
+    @image = Image.new
   end
+
+  def create
+    @image = Image.new(image_params)
+    @image.save
+
+    respond_to do |format|
+      format.json { render json: { url: Refile.attachment_url(@image, :picture) } }
+    end
+  end
+end
+
+private
+
+def image_params
+  params.require(:image).permit(:name, :picture, :article_id, :user_id)
 end
