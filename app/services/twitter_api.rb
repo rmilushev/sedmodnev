c20077900ca584f3d42d77ade1d29882ed33dd84
Rumen Milushev <r@vert.bg>
2016-07-23 15:20:43 +0300

    Add twitter summary card for articles

---
+++ app/services/twitter_api.rb
@@ -0,0 +1,10 @@
+class TwitterAPI
+  def client
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key = ENV['TW_CONSUMER_KEY']
+      config.consumer_secret = ENV['TW_CONSUMER_SECRET']
+      config.access_token = ENV['TW_ACCESS_TOKEN']
+      config.access_token_secret = ENV['TW_ACCESS_TOKEN_SECRET']
+    end
+  end
+end
