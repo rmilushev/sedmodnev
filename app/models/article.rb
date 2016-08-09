baf9eecce3a64465d4d82060039ac3593a50e42f
Rumen Milushev <r@vert.bg>
2016-08-09 16:21:10 +0300

    Add scope for articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ActiveRecord::Base
   default_scope { order('created_at DESC') }
+  scope :recent, -> { where('created_at >= ?', 3.days.ago) }
   validates :title, presence: true
   attachment :article_image
 end
