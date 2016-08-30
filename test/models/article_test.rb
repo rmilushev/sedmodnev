8406be4a0318c4508e26d693fa70b7604b7ff1f6
Rumen Milushev <r@vert.bg>
2016-08-30 23:39:35 +0300

    Set up minitest and create first unit test

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -2,14 +2,14 @@ require 'test_helper'
 
 class ArticleTest < ActiveSupport::TestCase
   def article
-    @article ||= articles(:one)
-  end
-  test 'shoud not save article without a title' do
-    article_db = Article.new
-    assert_not article_db.save, 'Saved an article without title'
+    @article ||= articles :one
   end
 
-  test 'should have article with title Article_1' do
-    assert_equal article.title, 'First article'
+  def test_valid
+    assert article.valid?
+  end
+
+  test 'article has a title' do
+    assert_equal article.title, 'Article numero uno'
   end
 end
