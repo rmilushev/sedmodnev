fed2a6bdaa53ad622f1324f19f0363df563dd66a
Rumen Milushev <r@vert.bg>
2016-09-13 15:13:51 +0300

    Config action mailer for mailcatcher

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,11 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: '127.0.0.1',
+    port: 1025
+  }
   config.action_mailer.raise_delivery_errors = false
 
   # Print deprecation notices to the Rails logger.
