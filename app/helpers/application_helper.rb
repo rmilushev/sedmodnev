89bba846e4fd877fd2870f570922cc23e3a968b9
Rumen Milushev <r@vert.bg>
2017-09-18 00:39:55 +0300

    Refactor styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def tags
-    return (ActsAsTaggebleOn::Tag.all - @current_tag).sample(4) if @current_tag
-    ActsAsTaggableOn::Tag.all.sample(5)
+    return Tag.all.except(@current_tag).sample(4) if @current_tag
+    Tag.all.sample(5)
   end
 end
