4a9716b6752935add1cab3d4853f126e11fe3234
Rumen Milushev <r@vert.bg>
2017-10-17 22:07:29 +0300

    Change index layout

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -2,4 +2,8 @@ module ArticlesHelper
   def tick_bool(val)
     val ? (haml_tag :i, class: 'fa fa-check-square-o') : (haml_tag :i, class: 'fa fa-times')
   end
+
+  def artpic(klass, height, width)
+    image_tag attachment_url(klass, :article_image, :fit, height, width), class: 'img-responsive'
+  end
 end
