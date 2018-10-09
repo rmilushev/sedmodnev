89c790fa902a75c0a58d18e0881a3c7d988145ce
Rumen Milushev <r@vert.bg>
2018-10-09 15:32:17 +0300

    Improve items design

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -4,7 +4,7 @@ module ArticlesHelper
   end
 
   def artpic(klass, height, width)
-    image_tag attachment_url(klass, :article_image, :fit, height, width), class: 'img-responsive'
+    image_tag attachment_url(klass, :article_image, :fit, height, width), class: 'img-responsive img-round'
   end
 
   def artitem_id(i)
