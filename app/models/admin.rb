5ab846c078e8c3fc45ce22186660abb49250dba1
Rumen Milushev <r@vert.bg>
2021-06-01 20:14:28 +0300

    Mute admin registration

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,8 +1,7 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-  # has_many :images
+  devise :database_authenticatable,
+          :rememberable, :trackable, :validatable
+          #:recoverable, :registerable,
 end
